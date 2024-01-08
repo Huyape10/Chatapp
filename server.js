cfc964fb443fba4144128fd0077dccfe4f6c63f3
Glitch (glitch-hello-node) <none>
2024-01-08 03:57:49 +0000

    👧🌽 Checkpoint ./package.json:60431628/1298 ./server.js:60431628/250

--- server.js
+++ server.js
@@ -4,10 +4,21 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 
+const {
+  Server
+} = require ('socket.io')
+
+const io= new Server(server)
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname+'/index.html')
 });
-
+io.on('connection',(socket)=>{
+  console.log('user connected')
+}
+      )
+      
+      
 server.listen(3000, () => {
     console.log('listening on port 3000');
 });
