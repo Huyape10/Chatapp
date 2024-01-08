7dc88ccbb02d805d4662a61b40f5cf0c926e0cad
Glitch (glitch-hello-node) <none>
2024-01-08 21:25:57 +0000

    🙎☕️ Checkpoint ./server.js:60431628/1040 ./index.html:60431628/1093

--- server.js
+++ server.js
@@ -5,11 +5,8 @@ const app = express();
 const server = createServer(app);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+    res.sendFile(__dirname + '/index.html')
 });
-
-
-
 server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+  process.stdout.write('server running at http://localhost:3000\n');
 });
